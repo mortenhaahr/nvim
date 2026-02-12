7c0243075264e421a51bf30b55f6c9dc8cdbdd7b
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2026-02-12 07:10:06 +0100

    Bugfix

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,7 +1,6 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    preset = "default",
     keymap = {
       ["<C-Space>"] = { "accept" }, -- Accepts selected or falls back
       ["<C-s>"] = { "show", "show_documentation", "hide_documentation" },
