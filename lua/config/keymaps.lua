06c50a731f1ee7236879b2ea3a601d0757ef7193
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2026-02-12 13:07:10 +0100

    Keybindings for diagnostics

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "ø", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+vim.keymap.set("n", "æ", function()
+  vim.diagnostic.goto_prev()
+end, opts)
