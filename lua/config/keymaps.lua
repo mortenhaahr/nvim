6fffdbedf3400c8866bccf5dcd12b8f4d2abb72e
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2026-03-18 11:40:06 +0100

    Git amend and no-edit keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,10 @@ vim.keymap.set("n", "æ", function()
 end, { desc = "Diagnostics goto prev" })
 
 vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "Git commit (Fugitive)" })
+vim.keymap.set("n", "<leader>ga", "<cmd>Git commit --amend<CR>", { desc = "Git commit amend (Fugitive)" })
+vim.keymap.set(
+  "n",
+  "<leader>gn",
+  "<cmd>Git commit --amend --no-edit<CR>",
+  { desc = "Git commit no-edit amend (Fugitive)" }
+)
